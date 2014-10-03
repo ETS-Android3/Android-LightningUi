09f4a639b451a593504e2aaa4238eafa6c05a2c9
Callum Taylor <callum@3sidedcube.com>
2014-10-03 13:31:17 +0100

    Added base text list item holder class

---
+++ src/main/java/com/cube/storm/ui/view/holder/TextListItemHolder.java
@@ -0,0 +1,24 @@
+package com.cube.storm.ui.view.holder;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cube.storm.ui.model.list.TextListItem;
+
+/**
+ * // TODO: Add class description
+ *
+ * @author Callum Taylor
+ * @project Storm Test
+ */
+public class TextListItemHolder extends Holder<TextListItem>
+{
+	@Override public View createView(ViewGroup parent)
+	{
+		return null;
+	}
+
+	@Override public void populateView(TextListItem model)
+	{
+	}
+}
