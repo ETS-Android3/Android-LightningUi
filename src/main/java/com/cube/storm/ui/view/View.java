f6c6eb6c1f30ae66e904335614c2bdeabcef77c9
Callum Taylor <callum@3sidedcube.com>
2014-10-03 13:31:34 +0100

    Added text list item into view enum

--- src/main/java/com/cube/storm/ui/view/View.java
+++ src/main/java/com/cube/storm/ui/view/View.java
@@ -1,5 +1,7 @@
 package com.cube.storm.ui.view;
 
+import com.cube.storm.ui.view.holder.TextListItemHolder;
+
 /**
  * This is the enum class with the list of all supported view types, their model classes and their
  * corresponding view holder class. This list should not be modified or overridden
@@ -9,14 +11,33 @@ package com.cube.storm.ui.view;
  */
 public enum View
 {
+	TextListItem(com.cube.storm.ui.model.list.TextListItem.class, TextListItemHolder.class),
+
+	// Not used
 	Default(Object.class, Object.class);
 
-	public Class model;
-	public Class holder;
+	private Class model;
+	private Class holder;
 
 	private View(Class model, Class holder)
 	{
 		this.model = model;
 		this.holder = holder;
+	}
+
+	/**
+	 * @return Gets the holder class of the view
+	 */
+	public Class getHolderClass()
+	{
+		return holder;
+	}
+
+	/**
+	 * @return Gets the model class of the view
+	 */
+	public Class getModelClass()
+	{
+		return model;
 	}
 }
