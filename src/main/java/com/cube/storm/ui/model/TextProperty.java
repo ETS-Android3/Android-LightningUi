b70f429771bcfef728a5b78756bcebc5f72c67e9
Callum Taylor <callum@3sidedcube.com>
2014-10-03 11:54:38 +0100

    Added text property class

---
+++ src/main/java/com/cube/storm/ui/model/TextProperty.java
@@ -0,0 +1,28 @@
+package com.cube.storm.ui.model;
+
+import android.os.Parcel;
+
+import lombok.Getter;
+
+/**
+ * Text property class. This class has a content string which can either be a language coded string
+ * or a string. If a language manager is set, the string is replaced with it's lookup equivalent from
+ * the language manager, else it is left. Do not set a language manager to disable this behaviour.
+ *
+ * @author Callum Taylor
+ * @project StormUI
+ */
+public class TextProperty extends Model
+{
+	@Getter private String content;
+
+	@Override public int describeContents()
+	{
+		return 0;
+	}
+
+	@Override public void writeToParcel(Parcel dest, int flags)
+	{
+
+	}
+}
