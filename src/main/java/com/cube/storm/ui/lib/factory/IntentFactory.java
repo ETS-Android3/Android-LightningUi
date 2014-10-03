a826c7d3856a38cc338a9ae930622ae3fea30fb9
Callum Taylor <callum@3sidedcube.com>
2014-10-03 11:10:26 +0100

    Added base IntentFactory class

---
+++ src/main/java/com/cube/storm/ui/lib/factory/IntentFactory.java
@@ -0,0 +1,13 @@
+package com.cube.storm.ui.lib.factory;
+
+/**
+ * This is the factory class which is used by Storm to decide which activity/fragments to instantiate
+ * based on the source file's class type, name, or Uri.
+ *
+ * @author Callum Taylor
+ * @project StormUI
+ */
+public class IntentFactory
+{
+
+}
