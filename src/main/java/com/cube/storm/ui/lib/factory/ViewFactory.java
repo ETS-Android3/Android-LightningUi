946c49f5a678721b10132e90c54afda3393a7474
Callum Taylor <callum@3sidedcube.com>
2014-10-03 11:27:21 +0100

    Added base view factory class

---
+++ src/main/java/com/cube/storm/ui/lib/factory/ViewFactory.java
@@ -0,0 +1,13 @@
+package com.cube.storm.ui.lib.factory;
+
+/**
+ * This is the factory class which is used by Storm to help with getting the correct view holder/controller
+ * for a specific view class name.
+ *
+ * @author Callum Taylor
+ * @project StormUI
+ */
+public class ViewFactory
+{
+
+}
