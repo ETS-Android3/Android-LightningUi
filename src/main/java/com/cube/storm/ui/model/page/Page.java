ee64a86ff4a5b82796917c995361543fdf6d1dad
Callum Taylor <callum@3sidedcube.com>
2014-10-06 11:08:16 +0100

    Added more generic to the get children method

--- src/main/java/com/cube/storm/ui/model/page/Page.java
+++ src/main/java/com/cube/storm/ui/model/page/Page.java
@@ -18,5 +18,5 @@ public abstract class Page extends Model
 	@Getter protected TextProperty title;
 	@Getter protected String name;
 
-	public abstract Collection<Model> getChildren();
+	public abstract Collection<? extends Model> getChildren();
 }
