fa5111476f1572f3de9f9eca750dcdb47a642012
Callum Taylor <callum@3sidedcube.com>
2014-10-06 12:20:29 +0100

    Added basic method to resolve activity intent for page data

--- src/main/java/com/cube/storm/ui/lib/factory/IntentFactory.java
+++ src/main/java/com/cube/storm/ui/lib/factory/IntentFactory.java
@@ -1,8 +1,11 @@
 package com.cube.storm.ui.lib.factory;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import com.cube.storm.ui.activity.StormActivity;
 import com.cube.storm.ui.data.FragmentIntent;
 import com.cube.storm.ui.fragment.StormListFragment;
 import com.cube.storm.ui.model.page.ListPage;
@@ -32,6 +35,27 @@ public abstract class IntentFactory
 		if (pageData instanceof ListPage)
 		{
 			intent = new FragmentIntent(StormListFragment.class, pageData.getTitle() != null ? pageData.getTitle().getContent() : "", null);
+			return intent;
+		}
+
+		return null;
+	}
+
+	/**
+	 * Returns an activity intent for a specific page
+	 *
+	 * @param pageData The page to use to decide the activity that is used
+	 *
+	 * @return The intent, or null if one was not suitable enough
+	 */
+	@Nullable
+	public Intent getIntentForPage(@NonNull Context context, @NonNull Page pageData)
+	{
+		Intent intent;
+
+		if (pageData instanceof ListPage)
+		{
+			intent = new Intent(context, StormActivity.class);
 			return intent;
 		}
 
