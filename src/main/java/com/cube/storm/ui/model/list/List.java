b13dd8ff1254380ccf5ca7bb5d292f6b0640f06c
Callum Taylor <callum@3sidedcube.com>
2014-10-06 10:22:19 +0100

    Added base List model

---
+++ src/main/java/com/cube/storm/ui/model/list/List.java
@@ -0,0 +1,81 @@
+package com.cube.storm.ui.model.list;
+
+import android.os.Parcel;
+
+import com.cube.storm.ui.model.Model;
+import com.cube.storm.ui.model.property.TextProperty;
+
+import java.util.ArrayList;
+
+import lombok.Getter;
+
+/**
+ * A model which hosts an array set of child {@link com.cube.storm.ui.model.list.ListItem} models. Used
+ * for displaying a sub set of lists within the list view.
+ *
+ * This is purely a data based model and is never used when displaying content in the list, instead
+ * the model is processed into 2 separate models, {@link com.cube.storm.ui.model.list.List.ListHeader} and {@link com.cube.storm.ui.model.list.List.ListFooter}
+ *
+ * @author Callum Taylor
+ * @project StormUI
+ */
+public class List extends Model
+{
+	@Getter private TextProperty header;
+	@Getter private TextProperty footer;
+	@Getter private ArrayList<ListItem> children;
+
+	/**
+	 * This is the model used when displaying the list in the adapter. The list model is processed
+	 * into a list of subviews from its {@link #children} and a {@link com.cube.storm.ui.model.list.List.ListHeader} and {@link com.cube.storm.ui.model.list.List.ListFooter}
+	 * from its {@link #header} and {@link #footer}. The reason why it's done like this is because
+	 * we don't want to track a single object in a list which is referenced in multiple places, so
+	 * we instantiate 2 'place holder' models to handle it. {@link com.cube.storm.ui.model.list.List} isn't
+	 * actually a visible view, where as it's members is.
+	 */
+	public static class ListHeader extends Model
+	{
+		@Getter private TextProperty header;
+
+		@Override public int describeContents()
+		{
+			return 0;
+		}
+
+		@Override public void writeToParcel(Parcel dest, int flags)
+		{
+		}
+	}
+
+	/**
+	 * This is the model used when displaying the list in the adapter. The list model is processed
+	 * into a list of subviews from its {@link #children} and a {@link com.cube.storm.ui.model.list.List.ListHeader} and {@link com.cube.storm.ui.model.list.List.ListFooter}
+	 * from its {@link #header} and {@link #footer}. The reason why it's done like this is because
+	 * we don't want to track a single object in a list which is referenced in multiple places, so
+	 * we instantiate 2 'place holder' models to handle it. {@link com.cube.storm.ui.model.list.List} isn't
+	 * actually a visible view, where as it's members is.
+	 */
+	public static class ListFooter extends Model
+	{
+		@Getter private TextProperty footer;
+
+		@Override public int describeContents()
+		{
+			return 0;
+		}
+
+		@Override public void writeToParcel(Parcel dest, int flags)
+		{
+		}
+	}
+
+	@Override public int describeContents()
+	{
+		return 0;
+	}
+
+	@Override public void writeToParcel(Parcel dest, int flags)
+	{
+
+	}
+}
