b6d23a298abe1c3f67269118100a6a521e23dc83
Alan Le Fournis <alan@3sidedcube.com>
2014-10-07 11:09:01 +0100

    Added title list item into view enum

--- src/main/java/com/cube/storm/ui/view/View.java
+++ src/main/java/com/cube/storm/ui/view/View.java
@@ -6,6 +6,7 @@ import com.cube.storm.ui.view.holder.ImageListItemHolder;
 import com.cube.storm.ui.view.holder.ListFooterHolder;
 import com.cube.storm.ui.view.holder.ListHeaderHolder;
 import com.cube.storm.ui.view.holder.TextListItemHolder;
+import com.cube.storm.ui.view.holder.TitleListItemHolder;
 
 /**
  * This is the enum class with the list of all supported view types, their model classes and their
@@ -26,6 +27,7 @@ public enum View
 	List(com.cube.storm.ui.model.list.List.class, null),
 	TextListItem(com.cube.storm.ui.model.list.TextListItem.class, TextListItemHolder.class),
 	ImageListItem(com.cube.storm.ui.model.list.ImageListItem.class, ImageListItemHolder.class),
+	TitleListItem(com.cube.storm.ui.model.list.TitleListItem.class, TitleListItemHolder.class),
 
 	ListPage(com.cube.storm.ui.model.page.ListPage.class, null),
 
