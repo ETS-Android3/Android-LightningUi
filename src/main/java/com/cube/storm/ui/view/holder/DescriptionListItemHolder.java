10c7ab09d058fbde8c89655842269da03cd37b79
Alan Le Fournis <alan@3sidedcube.com>
2014-10-07 11:23:38 +0100

    Added holder population

--- src/main/java/com/cube/storm/ui/view/holder/DescriptionListItemHolder.java
+++ src/main/java/com/cube/storm/ui/view/holder/DescriptionListItemHolder.java
@@ -1,8 +1,10 @@
 package com.cube.storm.ui.view.holder;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.cube.storm.ui.R;
 import com.cube.storm.ui.model.list.DescriptionListItem;
@@ -15,14 +17,33 @@ import com.cube.storm.ui.model.list.DescriptionListItem;
  */
 public class DescriptionListItemHolder extends Holder<DescriptionListItem>
 {
+	protected TextView title;
+	protected TextView description;
+
 	@Override public View createView(ViewGroup parent)
 	{
 		View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.description_list_item_view, parent, false);
+		title = (TextView)view.findViewById(R.id.title);
+		description = (TextView)view.findViewById(R.id.description);
 
 		return view;
 	}
 
 	@Override public void populateView(DescriptionListItem model)
 	{
+		if (model.getTitle() != null)
+		{
+			title.setText(model.getTitle().getContent());
+		}
+
+		if (model.getDescription() != null && !TextUtils.isEmpty(model.getDescription().getContent()))
+		{
+			description.setText(model.getDescription().getContent());
+			description.setVisibility(View.VISIBLE);
+		}
+		else
+		{
+			description.setVisibility(View.GONE);
+		}
 	}
 }
