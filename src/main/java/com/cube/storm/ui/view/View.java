d7124a8c18bfad1085e0b0d169921bec09164ab0
Alan Le Fournis <alan@3sidedcube.com>
2014-10-07 09:44:47 +0100

    Added image list item into view enum

--- src/main/java/com/cube/storm/ui/view/View.java
+++ src/main/java/com/cube/storm/ui/view/View.java
@@ -2,6 +2,7 @@ package com.cube.storm.ui.view;
 
 import com.cube.storm.ui.model.Model;
 import com.cube.storm.ui.view.holder.Holder;
+import com.cube.storm.ui.view.holder.ImageListItemHolder;
 import com.cube.storm.ui.view.holder.ListFooterHolder;
 import com.cube.storm.ui.view.holder.ListHeaderHolder;
 import com.cube.storm.ui.view.holder.TextListItemHolder;
@@ -24,6 +25,7 @@ public enum View
 
 	List(com.cube.storm.ui.model.list.List.class, null),
 	TextListItem(com.cube.storm.ui.model.list.TextListItem.class, TextListItemHolder.class),
+	ImageListItem(com.cube.storm.ui.model.list.ImageListItem.class, ImageListItemHolder.class),
 
 	ListPage(com.cube.storm.ui.model.page.ListPage.class, null),
 
