522f261d79890bc566b47454686867a46fedc2a5
Alan Le Fournis <alan@3sidedcube.com>
2014-10-07 15:38:48 +0100

    Added base checkable list item model

---
+++ src/main/java/com/cube/storm/ui/model/list/CheckableListItem.java
@@ -0,0 +1,28 @@
+package com.cube.storm.ui.model.list;
+
+import android.os.Parcel;
+
+import com.cube.storm.ui.model.property.TextProperty;
+
+import lombok.Getter;
+
+/**
+ * A view model with a description property
+ *
+ * @author Callum Taylor
+ * @project StormUI
+ */
+public class CheckableListItem extends TitleListItem
+{
+	@Getter private TextProperty title;
+
+	@Override public int describeContents()
+	{
+		return 0;
+	}
+
+	@Override public void writeToParcel(Parcel dest, int flags)
+	{
+
+	}
+}
