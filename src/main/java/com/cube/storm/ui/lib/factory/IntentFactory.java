7f0bcfb95520d562bd421f7a8b55fcbc111a77de
Callum Taylor <callum@3sidedcube.com>
2014-10-08 12:52:25 +0100

    Added methods to resolve a fragment or intent from a page descriptor

--- src/main/java/com/cube/storm/ui/lib/factory/IntentFactory.java
+++ src/main/java/com/cube/storm/ui/lib/factory/IntentFactory.java
@@ -5,9 +5,12 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import com.cube.storm.UiSettings;
 import com.cube.storm.ui.activity.StormActivity;
 import com.cube.storm.ui.data.FragmentIntent;
 import com.cube.storm.ui.fragment.StormListFragment;
+import com.cube.storm.ui.model.Model;
+import com.cube.storm.ui.model.descriptor.PageDescriptor;
 import com.cube.storm.ui.model.page.ListPage;
 import com.cube.storm.ui.model.page.Page;
 
@@ -21,13 +24,15 @@ import com.cube.storm.ui.model.page.Page;
 public abstract class IntentFactory
 {
 	/**
+	 * @deprecated You should not load a fragment intent from an already-instantiated page, use {@link #getFragmentIntentForPageDescriptor(com.cube.storm.ui.model.descriptor.PageDescriptor)} instead
+	 *
 	 * Returns a fragment intent for a specific page
 	 *
 	 * @param pageData The page to use to decide the fragment that is used
 	 *
 	 * @return The fragment intent, or null if one was not suitable enough
 	 */
-	@Nullable
+	@Nullable @Deprecated
 	public FragmentIntent getFragmentIntentForPage(@NonNull Page pageData)
 	{
 		FragmentIntent intent;
@@ -42,18 +47,67 @@ public abstract class IntentFactory
 	}
 
 	/**
-	 * Returns an activity intent for a specific page
+	 * @deprecated You should not load an intent from an already-instantiated page, use {@link #getIntentForPageDescriptor(android.content.Context, com.cube.storm.ui.model.descriptor.PageDescriptor)} instead
+	 *
+ 	 * Returns an activity intent for a specific page
 	 *
 	 * @param pageData The page to use to decide the activity that is used
 	 *
 	 * @return The intent, or null if one was not suitable enough
 	 */
-	@Nullable
+	@Nullable @Deprecated
 	public Intent getIntentForPage(@NonNull Context context, @NonNull Page pageData)
 	{
 		Intent intent;
 
 		if (pageData instanceof ListPage)
+		{
+			intent = new Intent(context, StormActivity.class);
+			return intent;
+		}
+
+		return null;
+	}
+
+	/**
+	 * Loads a fragment intent from a page descriptor by finding the model of the page type defined in {@link com.cube.storm.ui.model.descriptor.PageDescriptor#getType()} in the
+	 * {@link com.cube.storm.ui.view.View} enum.
+	 *
+	 * @param pageDescriptor The page descriptor to load from
+	 *
+	 * @return The intent, or null if one was not suitable enough
+	 */
+	@Nullable
+	public FragmentIntent getFragmentIntentForPageDescriptor(@NonNull PageDescriptor pageDescriptor)
+	{
+		FragmentIntent intent;
+		Class<? extends Model> pageType = UiSettings.getInstance().getViewFactory().getModelForView(pageDescriptor.getType());
+
+		if (ListPage.class.isAssignableFrom(pageType))
+		{
+			intent = new FragmentIntent(StormListFragment.class, null);
+			return intent;
+		}
+
+		return null;
+	}
+
+	/**
+	 * Loads an intent from a page descriptor by finding the model of the page type defined in {@link com.cube.storm.ui.model.descriptor.PageDescriptor#getType()} in the
+	 * {@link com.cube.storm.ui.view.View} enum.
+	 *
+	 * @param context The context used to create the intent
+	 * @param pageDescriptor The page descriptor to load from
+	 *
+	 * @return The intent, or null if one was not suitable enough
+	 */
+	@Nullable
+	public Intent getIntentForPageDescriptor(@NonNull Context context, @NonNull PageDescriptor pageDescriptor)
+	{
+		Intent intent;
+		Class<? extends Model> pageType = UiSettings.getInstance().getViewFactory().getModelForView(pageDescriptor.getType());
+
+		if (ListPage.class.isAssignableFrom(pageType))
 		{
 			intent = new Intent(context, StormActivity.class);
 			return intent;
