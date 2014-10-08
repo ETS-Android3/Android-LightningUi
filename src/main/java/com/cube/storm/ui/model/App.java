8bec8754c1b89f8c0e5b5833574e02edd6365eef
Callum Taylor <callum@3sidedcube.com>
2014-10-08 12:41:34 +0100

    Added app into ui settings and added documentation to model

--- src/main/java/com/cube/storm/ui/model/App.java
+++ src/main/java/com/cube/storm/ui/model/App.java
@@ -9,7 +9,29 @@ import java.util.Collection;
 import lombok.Getter;
 
 /**
- * // TODO: Add class description
+ * App class is a global data set which defines the content in the module handled by StormContent.
+ *
+ * This class is used when loading pages or finding out what type a page is before loading it to prevent
+ * having to load up a whole file to decide what to do with it. This is primarily used with {@link com.cube.storm.ui.lib.factory.IntentFactory}
+ * to decide what fragment/activity to load for a page Uri or name.
+ * <br />
+ * This class also has a vector Uri which tells you what the first page should be when starting the app.
+ * <br />
+ * You must load/create this class in your application singleton to prevent it from being cleared
+ * from memory whilst the app is still open.
+ * <br /><br />
+ * Example load
+ * <pre>
+ * {@code
+ * String appUri = "assets://app.json"; // This can be anything, it is loaded and dealt with by the FileFactory defined in UiSettings
+ * App app = UiSettings.getInstance().getViewBuilder().buildApp(Uri.parse(appUri));
+ *
+ * if (app != null)
+ * {
+ * 	UiSettings.getInstance().setApp(app);
+ * }
+ * }
+ * </pre>
  *
  * @author Callum Taylor
  * @project StormUI
