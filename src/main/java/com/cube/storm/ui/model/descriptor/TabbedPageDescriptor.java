e5699eda413dad765cfc79d2e0c26d7c2b2c680c
Callum Taylor <callum@3sidedcube.com>
2014-10-09 10:23:07 +0100

    Added tabbed page descriptor and collection models

---
+++ src/main/java/com/cube/storm/ui/model/descriptor/TabbedPageDescriptor.java
@@ -0,0 +1,24 @@
+package com.cube.storm.ui.model.descriptor;
+
+import android.os.Parcel;
+
+/**
+ * // TODO: Add class description
+ *
+ * @author Callum Taylor
+ * @project StormUI
+ */
+public class TabbedPageDescriptor extends PageDescriptor
+{
+	//@Getter protected TabBarItem tabBarItem;
+
+	@Override public int describeContents()
+	{
+		return 0;
+	}
+
+	@Override public void writeToParcel(Parcel dest, int flags)
+	{
+
+	}
+}
---
+++ src/main/java/com/cube/storm/ui/model/page/TabbedPageCollection.java
@@ -0,0 +1,30 @@
+package com.cube.storm.ui.model.page;
+
+import android.os.Parcel;
+
+import com.cube.storm.ui.model.descriptor.TabbedPageDescriptor;
+
+import java.util.Collection;
+
+import lombok.Getter;
+
+/**
+ * // TODO: Add class description
+ *
+ * @author Callum Taylor
+ * @project StormUI
+ */
+public class TabbedPageCollection extends PageCollection
+{
+	@Getter protected Collection<TabbedPageDescriptor> pages;
+
+	@Override public int describeContents()
+	{
+		return 0;
+	}
+
+	@Override public void writeToParcel(Parcel dest, int flags)
+	{
+
+	}
+}
