59855df90de14875680c2f95bd3d08e32e1d1e5b
Callum Taylor <callum@3sidedcube.com>
2014-10-10 15:25:13 +0100

    Added text processor to deal with TextProperty#content member

---
+++ src/main/java/com/cube/storm/ui/lib/processor/TextProcessor.java
@@ -0,0 +1,29 @@
+package com.cube.storm.ui.lib.processor;
+
+import android.support.annotation.Nullable;
+
+import com.cube.storm.ui.model.property.TextProperty;
+import com.cube.storm.util.lib.processor.Processor;
+
+/**
+ * Processor class used for dealing with {@link com.cube.storm.ui.model.property.TextProperty#content} strings by processing them
+ * into a useful string.
+ *
+ * This class defaults to outputting the input
+ *
+ * @author Callum Taylor
+ * @project StormUI
+ */
+public class TextProcessor extends Processor<TextProperty, String>
+{
+	@Nullable
+	@Override public String process(@Nullable TextProperty textProperty)
+	{
+		if (textProperty == null)
+		{
+			return "";
+		}
+
+		return String.valueOf(textProperty.getContent());
+	}
+}
