ccaab4a9d3358a6165b133e355903c5efa8805e5
Luke Reed <luke@3sidedcube.com>
2014-11-04 10:57:11 +0000

    Added the animated image list item

---
+++ src/main/java/com/cube/storm/ui/model/list/AnimatedImageListItem.java
@@ -0,0 +1,22 @@
+package com.cube.storm.ui.model.list;
+
+import android.os.Parcel;
+
+/**
+ * // TODO: Add class description
+ *
+ * @author Callum Taylor
+ * @project Storm
+ */
+public class AnimatedImageListItem extends ListItem
+{
+	@Override public int describeContents()
+	{
+		return 0;
+	}
+
+	@Override public void writeToParcel(Parcel dest, int flags)
+	{
+
+	}
+}
