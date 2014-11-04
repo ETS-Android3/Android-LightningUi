e6bc090ba38352c7cdaf86f5080711c4fca16ba7
Luke Reed <luke@3sidedcube.com>
2014-11-04 11:16:16 +0000

    Added animated list item holder

---
+++ src/main/java/com/cube/storm/ui/view/holder/AnimatedImageListItemHolder.java
@@ -0,0 +1,26 @@
+package com.cube.storm.ui.view.holder;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cube.storm.ui.model.list.AnimatedImageListItem;
+
+/**
+ * // TODO: Add class description
+ *
+ * @author Luke Reed
+ * @project Storm
+ */
+public class AnimatedImageListItemHolder extends Holder<AnimatedImageListItem>
+{
+
+	@Override public View createView(ViewGroup parent)
+	{
+		return null;
+	}
+
+	@Override public void populateView(AnimatedImageListItem model)
+	{
+
+	}
+}
