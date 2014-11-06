cc51295dcc45b434da58c0e9a346d9f46b61f8b4
Matt Allen <mattallen092@gmail.com>
2014-11-06 14:23:45 +0000

    Ported animated image list item over to new holder for recycler view

--- src/main/java/com/cube/storm/ui/view/holder/AnimatedImageListItemHolder.java
+++ src/main/java/com/cube/storm/ui/view/holder/AnimatedImageListItemHolder.java
@@ -28,84 +28,94 @@ import java.util.TimerTask;
  * @author Luke Reed
  * @project Storm
  */
-public class AnimatedImageListItemHolder extends Holder<AnimatedImageListItem>
+public class AnimatedImageListItemHolder extends ViewHolderController
 {
-	private static final int MSG_UPDATE = 1;
 
-	private ImageView image;
-	private AnimatedImageListItem model; //This is bad mmkay, controllers will come with the recycler view
-	private int currentIndex = 0;
-	private Timer timer;
-	private Handler handler;
-
-	@Override public View createView(ViewGroup parent)
+	@Override public ViewHolder createViewHolder(ViewGroup parent)
 	{
 		View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.animated_image_list_item_view, parent, false);
-		image = (ImageView)view.findViewById(R.id.image_view);
+		mViewHolder = new AnimatedImageListItemViewHolder(view);
 
-		return view;
+		return mViewHolder;
 	}
 
-	@Override public void populateView(AnimatedImageListItem model)
+	private class AnimatedImageListItemViewHolder extends ViewHolder<AnimatedImageListItem>
 	{
-		if (this.model == null)
+		private static final int MSG_UPDATE = 1;
+
+		private ImageView image;
+		private AnimatedImageListItem model; //This is bad mmkay, controllers will come with the recycler view
+		private int currentIndex = 0;
+		private Timer timer;
+		private Handler handler;
+
+		public AnimatedImageListItemViewHolder(View view)
 		{
-			handler = new Handler()
+			super(view);
+			image = (ImageView)view.findViewById(R.id.image_view);
+		}
+
+		@Override public void populateView(AnimatedImageListItem model)
+		{
+			if (this.model == null)
 			{
-				@Override public void handleMessage(Message msg)
+				handler = new Handler()
 				{
-					if (msg.what == MSG_UPDATE)
+					@Override public void handleMessage(Message msg)
 					{
-						updateView();
+						if (msg.what == MSG_UPDATE)
+						{
+							updateView();
+						}
 					}
-				}
-			};
-			this.model = model;
-			currentIndex = 0;
+				};
+				this.model = model;
+				currentIndex = 0;
 
-			updateView();
+				updateView();
+			}
 		}
-	}
 
-	private void updateView()
-	{
-		if (model.getImages() != null)
+		private void updateView()
 		{
-			if (currentIndex >= model.getImages().size())
+			if (model.getImages() != null)
 			{
-				currentIndex = 0;
-			}
-
-			ImageLoader.getInstance().displayImage(model.getImages().get(currentIndex).getSrc(), image);
-
-			currentIndex++;
-			if (currentIndex >= model.getImages().size())
-			{
-				currentIndex = 0;
-			}
-			getTimer().schedule(new TimerTask()
-			{
-				@Override public void run()
+				if (currentIndex >= model.getImages().size())
 				{
-					handler.sendEmptyMessage(MSG_UPDATE);
+					currentIndex = 0;
 				}
-			}, model.getImages().get(currentIndex).getDelay());
-		}
-	}
 
-	protected Timer getTimer()
-	{
-		if (timer == null)
-		{
-			if (image.getTag() != null && image.getTag() instanceof Timer)
-			{
-				timer = (Timer) image.getTag();
-			}
-			else
-			{
-				timer = new Timer("Animated Image List Item timer");
+				ImageLoader.getInstance().displayImage(model.getImages().get(currentIndex).getSrc(), image);
+
+				currentIndex++;
+				if (currentIndex >= model.getImages().size())
+				{
+					currentIndex = 0;
+				}
+				getTimer().schedule(new TimerTask()
+				{
+					@Override public void run()
+					{
+						handler.sendEmptyMessage(MSG_UPDATE);
+					}
+				}, model.getImages().get(currentIndex).getDelay());
 			}
 		}
-		return timer;
+
+		protected Timer getTimer()
+		{
+			if (timer == null)
+			{
+				if (image.getTag() != null && image.getTag() instanceof Timer)
+				{
+					timer = (Timer)image.getTag();
+				}
+				else
+				{
+					timer = new Timer("Animated Image List Item timer");
+				}
+			}
+			return timer;
+		}
 	}
 }
