6286f8a9c626438cab494fd3e8c15b46962cf35c
Alan Le Fournis <alan@3sidedcube.com>
2014-11-07 16:37:32 +0000

    Added Embedded link for Collection List Item

--- src/main/java/com/cube/storm/ui/model/list/collection/CollectionListItem.java
+++ src/main/java/com/cube/storm/ui/model/list/collection/CollectionListItem.java
@@ -3,6 +3,7 @@ package com.cube.storm.ui.model.list.collection;
 import android.os.Parcel;
 
 import com.cube.storm.ui.model.Model;
+import com.cube.storm.ui.model.property.LinkProperty;
 
 import java.util.Collection;
 
@@ -17,6 +18,7 @@ import lombok.Getter;
 public class CollectionListItem extends Model
 {
 	@Getter protected Collection<CollectionItem> cells;
+	@Getter protected Collection<LinkProperty> embeddedLinks;
 
 	@Override public int describeContents()
 	{
--- src/main/java/com/cube/storm/ui/view/holder/CollectionListItemHolder.java
+++ src/main/java/com/cube/storm/ui/view/holder/CollectionListItemHolder.java
@@ -4,12 +4,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.cube.storm.UiSettings;
 import com.cube.storm.ui.R;
 import com.cube.storm.ui.model.list.collection.CollectionItem;
 import com.cube.storm.ui.model.list.collection.CollectionListItem;
+import com.cube.storm.ui.model.property.LinkProperty;
 import com.cube.storm.ui.view.ViewClickable;
 
 import java.util.ArrayList;
@@ -26,11 +28,13 @@ public class CollectionListItemHolder extends Holder<CollectionListItem>
 	private ViewHolder holder;
 	private View view;
 	private LinearLayout linearLayout;
+	protected LinearLayout embeddedLinksContainer;
 
 	@Override public View createView(ViewGroup parent)
 	{
 		view = LayoutInflater.from(parent.getContext()).inflate(R.layout.collection_list_item_view, null, false);
 		linearLayout = (LinearLayout)view.findViewById(R.id.view_container);
+		embeddedLinksContainer = (LinearLayout)view.findViewById(R.id.embedded_links_container);
 		holder = new ViewHolder(view);
 
 		return view;
@@ -60,6 +64,34 @@ public class CollectionListItemHolder extends Holder<CollectionListItem>
 		}
 
 		view.refreshDrawableState();
+
+		if (model.getEmbeddedLinks() != null)
+		{
+			embeddedLinksContainer.removeAllViews();
+
+			for (LinkProperty linkProperty : model.getEmbeddedLinks())
+			{
+				final LinkProperty property = linkProperty;
+				View embeddedLinkView = LayoutInflater.from(embeddedLinksContainer.getContext()).inflate(R.layout.button_embedded_link, embeddedLinksContainer, false);
+
+				if (embeddedLinkView != null)
+				{
+					Button button = (Button)embeddedLinkView.findViewById(R.id.button);
+					button.setText(property.getTitle().getContent());
+
+					button.setOnClickListener(new OnClickListener()
+					{
+						@Override public void onClick(View v)
+						{
+							UiSettings.getInstance().getLinkHandler().handleLink(v.getContext(), property);
+						}
+					});
+
+					embeddedLinksContainer.setVisibility(View.VISIBLE);
+					embeddedLinksContainer.addView(button);
+				}
+			}
+		}
 	}
 
 	protected View getView(final CollectionItem model, View view)
