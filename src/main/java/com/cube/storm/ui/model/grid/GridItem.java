2f3c9ddee6f3ff6b51c3dee1a1b746a84e23ad08
Luke Reed <luke@3sidedcube.com>
2014-11-25 16:33:54 +0000

    Added span to the gridItem and built the gridViewHolder ready to be extended by other grid views

--- src/main/java/com/cube/storm/ui/model/grid/GridItem.java
+++ src/main/java/com/cube/storm/ui/model/grid/GridItem.java
@@ -1,6 +1,10 @@
 package com.cube.storm.ui.model.grid;
 
+import android.os.Parcel;
+
 import com.cube.storm.ui.model.Model;
+
+import lombok.Getter;
 
 /**
  * // TODO: Add class description
@@ -10,5 +14,15 @@ import com.cube.storm.ui.model.Model;
  */
 public abstract class GridItem extends Model
 {
+	@Getter protected Boolean spanned;
 
+	@Override public int describeContents()
+	{
+		return 0;
+	}
+
+	@Override public void writeToParcel(Parcel dest, int flags)
+	{
+
+	}
 }
