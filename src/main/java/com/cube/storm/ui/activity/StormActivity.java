4f6c9e3718f5e6fd65213a72c571509a17e98a55
Matt Allen <mattallen092@gmail.com>
2014-11-25 13:26:18 +0000

    Now using ActionBarActivity

--- src/main/java/com/cube/storm/ui/activity/StormActivity.java
+++ src/main/java/com/cube/storm/ui/activity/StormActivity.java
@@ -3,7 +3,7 @@ package com.cube.storm.ui.activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.text.TextUtils;
 import android.widget.Toast;
 
@@ -18,7 +18,7 @@ import com.cube.storm.ui.model.page.Page;
  * @author Callum Taylor
  * @project StormUI
  */
-public class StormActivity extends FragmentActivity
+public class StormActivity extends ActionBarActivity
 {
 	public static final String EXTRA_PAGE = "stormui.page";
 	public static final String EXTRA_URI = "stormui.uri";
@@ -82,7 +82,7 @@ public class StormActivity extends FragmentActivity
 			fragmentIntent.getArguments().putAll(getIntent().getExtras());
 
 			Fragment fragment = Fragment.instantiate(this, fragmentIntent.getFragment().getName(), fragmentIntent.getArguments());
-			getFragmentManager().beginTransaction().replace(R.id.fragment_holder, fragment).commit();
+			getSupportFragmentManager().beginTransaction().replace(R.id.fragment_holder, fragment).commit();
 
 			if (!TextUtils.isEmpty(fragmentIntent.getTitle()))
 			{
