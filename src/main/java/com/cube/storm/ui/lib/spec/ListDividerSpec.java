570381e524be77815f50597c2cebad3082d3d61c
Callum Taylor <callum@3sidedcube.com>
2015-03-19 16:13:58 +0000

    Created default list divider spec

---
+++ src/main/java/com/cube/storm/ui/lib/spec/ListDividerSpec.java
@@ -0,0 +1,40 @@
+package com.cube.storm.ui.lib.spec;
+
+import com.cube.storm.ui.model.Model;
+import com.cube.storm.ui.model.list.Divider;
+import com.cube.storm.ui.model.list.ListItem;
+import com.cube.storm.ui.model.list.StandardListItem;
+
+import java.util.List;
+
+/**
+ * Default {@link com.cube.storm.ui.lib.spec.DividerSpec} for {@link com.cube.storm.ui.controller.adapter.StormListAdapter}
+ *
+ * @author Callum Taylor
+ * @project LightningUi
+ */
+public class ListDividerSpec implements DividerSpec
+{
+	@Override public ListItem shouldAddDivider(int position, List<Model> items)
+	{
+		if (position > 0 && position < items.size() - 1)
+		{
+			if (items.get(position) instanceof com.cube.storm.ui.model.list.List.ListHeader)
+			{
+				return null;
+			}
+
+			if (items.get(position - 1) instanceof com.cube.storm.ui.model.list.List.ListHeader)
+			{
+				return new Divider();
+			}
+
+			if ((items.get(position + 1) instanceof StandardListItem))
+			{
+				return new Divider();
+			}
+		}
+
+		return null;
+	}
+}
