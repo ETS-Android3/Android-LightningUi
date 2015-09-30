13c48e5847da7058bd5c40271a2292f91e76d85e
Callum Taylor <callum@3sidedcube.com>
2015-09-30 16:28:15 +0100

    Added click listener for standard grid cell

--- library/src/main/java/com/cube/storm/ui/view/holder/grid/StandardGridItemViewHolder.java
+++ library/src/main/java/com/cube/storm/ui/view/holder/grid/StandardGridItemViewHolder.java
@@ -1,22 +1,18 @@
 package com.cube.storm.ui.view.holder.grid;
 
-import android.graphics.Bitmap;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.cube.storm.UiSettings;
 import com.cube.storm.ui.R;
 import com.cube.storm.ui.model.grid.StandardGridItem;
 import com.cube.storm.ui.model.property.LinkProperty;
+import com.cube.storm.ui.view.ImageView;
+import com.cube.storm.ui.view.TextView;
 import com.cube.storm.ui.view.holder.GridViewHolder;
 import com.cube.storm.ui.view.holder.ViewHolderFactory;
-import com.nostra13.universalimageloader.core.assist.FailReason;
-import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 
 /**
  * // TODO: Add class description
@@ -24,7 +20,7 @@ import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListene
  * @author Matt Allen
  * @project LightningUi
  */
-public class StandardGridItemViewHolder extends GridViewHolder<StandardGridItem>
+public class StandardGridItemViewHolder extends GridViewHolder<StandardGridItem> implements OnClickListener
 {
 	public static class Factory extends ViewHolderFactory
 	{
@@ -39,68 +35,32 @@ public class StandardGridItemViewHolder extends GridViewHolder<StandardGridItem>
 	protected TextView title;
 	protected TextView description;
 	protected LinkProperty link;
-	protected LinearLayout embeddedLinksContainer;
 
 	public StandardGridItemViewHolder(View view)
 	{
 		super(view);
 
+		view.setOnClickListener(this);
+
 		image = (ImageView)view.findViewById(R.id.image);
 		title = (TextView)view.findViewById(R.id.title);
 		description = (TextView)view.findViewById(R.id.description);
-		embeddedLinksContainer = (LinearLayout)view.findViewById(R.id.embedded_links_container);
 	}
 
 	@Override public void populateView(final StandardGridItem model)
 	{
 		link = model.getLink();
-		image.setVisibility(View.GONE);
 
-		if (model.getImage() != null)
+		image.populate(model.getImage());
+		title.populate(model.getTitle());
+		description.populate(model.getDescription());
+	}
+
+	@Override public void onClick(View v)
+	{
+		if (link != null)
 		{
-			UiSettings.getInstance().getImageLoader().displayImage(model.getImage().getSrc(), image, new SimpleImageLoadingListener()
-			{
-				@Override public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage)
-				{
-					if (loadedImage != null)
-					{
-						image.setVisibility(View.VISIBLE);
-					}
-				}
-
-				@Override public void onLoadingFailed(String imageUri, View view, FailReason failReason)
-				{
-					if (!imageUri.equalsIgnoreCase(model.getImage().getFallbackSrc()))
-					{
-						UiSettings.getInstance().getImageLoader().displayImage(model.getImage().getFallbackSrc(), image, this);
-					}
-				}
-			});
-		}
-
-		description.setVisibility(View.GONE);
-		title.setVisibility(View.GONE);
-
-		if (model.getTitle() != null)
-		{
-			String content = UiSettings.getInstance().getTextProcessor().process(model.getTitle().getContent());
-
-			if (!TextUtils.isEmpty(content))
-			{
-				title.setText(content);
-				title.setVisibility(View.VISIBLE);
-			}
-		}
-
-		if (model.getDescription() != null)
-		{
-			String content = UiSettings.getInstance().getTextProcessor().process(model.getDescription().getContent());
-
-			if (!TextUtils.isEmpty(content))
-			{
-				description.setText(content);
-				description.setVisibility(View.VISIBLE);
-			}
+			UiSettings.getInstance().getLinkHandler().handleLink(image.getContext(), link);
 		}
 	}
 }
