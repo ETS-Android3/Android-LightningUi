81e208529b2c41e8daaccbcb3fe31cc045f1dd16
Callum Taylor <callum@3sidedcube.com>
2016-05-18 15:07:32 +0100

    Added intent resolver class

---
+++ library/src/main/java/com/cube/storm/ui/lib/resolver/IntentResolver.java
@@ -0,0 +1,25 @@
+package com.cube.storm.ui.lib.resolver;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.cube.storm.ui.data.FragmentIntent;
+
+/**
+ * // TODO: Add class description
+ *
+ * @author Callum Taylor
+ * @project LightningUi
+ */
+public abstract class IntentResolver
+{
+	public Intent resolveIntent(Context context)
+	{
+		return null;
+	}
+
+	public FragmentIntent resolveFragmentIntent()
+	{
+		return null;
+	}
+}
