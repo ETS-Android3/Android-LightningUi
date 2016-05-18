2fa64b1085e50cf88233ec157bb69c53b8c9c2d9
Callum Taylor <callum@3sidedcube.com>
2016-05-18 15:21:50 +0100

    Added classdoc

--- library/src/main/java/com/cube/storm/ui/lib/resolver/IntentResolver.java
+++ library/src/main/java/com/cube/storm/ui/lib/resolver/IntentResolver.java
@@ -3,10 +3,12 @@ package com.cube.storm.ui.lib.resolver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.cube.storm.UiSettings;
 import com.cube.storm.ui.data.FragmentIntent;
 
 /**
- * // TODO: Add class description
+ * Class used to resolve a fragment/intent. Use this in conjunction with {@link UiSettings#getIntentResolver()} to register
+ * page id/uri/descriptors for resolution
  *
  * @author Callum Taylor
  * @project LightningUi
