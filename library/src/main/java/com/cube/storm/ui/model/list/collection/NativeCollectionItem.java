c75da0634b9aa082b4e39135bd857ebe2f0ce30a
Callum Taylor <callum@3sidedcube.com>
2016-06-21 08:52:00 +0100

    Added native collection item

---
+++ library/src/main/java/com/cube/storm/ui/model/list/collection/NativeCollectionItem.java
@@ -0,0 +1,34 @@
+package com.cube.storm.ui.model.list.collection;
+
+import android.os.Parcel;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * Native block item for collections
+ *
+ * @author Callum Taylor
+ */
+@NoArgsConstructor @AllArgsConstructor(suppressConstructorProperties = true)
+@Accessors(chain = true) @Data
+public class NativeCollectionItem extends CollectionItem
+{
+	public static String CLASS_NAME = "NativeCollectionItem";
+
+	{ this.className = CLASS_NAME; }
+
+	protected String name;
+
+	@Override public int describeContents()
+	{
+		return 0;
+	}
+
+	@Override public void writeToParcel(Parcel dest, int flags)
+	{
+
+	}
+}
