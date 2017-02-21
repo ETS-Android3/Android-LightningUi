419b7e466400fa92e7a15871df4f7e1755baecfb
Callum Taylor <callum@3sidedcube.com>
2017-02-21 12:05:20 +0000

    Added basic event hook class and setting

---
+++ library/src/main/java/com/cube/storm/ui/lib/EventHook.java
@@ -0,0 +1,34 @@
+package com.cube.storm.ui.lib;
+
+import android.content.Context;
+import android.view.View;
+
+import com.cube.storm.ui.model.Model;
+import com.cube.storm.ui.model.page.Page;
+import com.cube.storm.ui.model.property.LinkProperty;
+
+/**
+ * // TODO: Add class description
+ */
+public class EventHook
+{
+	public void onViewLinkedClicked(View itemView, Model view, LinkProperty link)
+	{
+
+	}
+
+	public void onViewClicked(View itemView, Model view)
+	{
+
+	}
+
+	public void onPageOpened(Context pageContext, Page page)
+	{
+
+	}
+
+	public void onPageClosed(Context pageContext, Page page)
+	{
+
+	}
+}
