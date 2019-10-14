ce2a4a3d2de38181f16993ac199eed13e0144016
Michael P-F <michael@3sidedcube.com>
2019-10-14 11:39:03 +0100

    ARCFA-226 - Change spotlight design to only include one spotlight per page.

--- library/src/main/java/com/cube/storm/ui/lib/adapter/StormSpotlightAdapter.java
+++ library/src/main/java/com/cube/storm/ui/lib/adapter/StormSpotlightAdapter.java
@@ -5,7 +5,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +13,8 @@ import com.cube.storm.ui.R;
 import com.cube.storm.ui.lib.EventHook;
 import com.cube.storm.ui.model.list.SpotlightListItem;
 import com.cube.storm.ui.model.property.SpotlightImageProperty;
-import com.cube.storm.ui.model.property.TextProperty;
 import com.cube.storm.ui.view.ImageView;
 import com.cube.storm.ui.view.TextView;
-import com.cube.storm.util.lib.processor.Processor;
 
 public class StormSpotlightAdapter extends PagerAdapter
 {
